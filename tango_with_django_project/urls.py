d06b39047fad30e2796489db9d367ecd2c3db4bd
Yigit Yildiz <2954056y@student.gla.ac.uk>
2025-02-10 01:20:00 +0000

    Chapter 3 Done with exercise and test passed.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rango import views
+from django.urls import include
+
+app_name = 'rango'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path('', views.index, name='index'),
+   path('rango/', include('rango.urls')),
+   path('admin/', admin.site.urls),
 ]
